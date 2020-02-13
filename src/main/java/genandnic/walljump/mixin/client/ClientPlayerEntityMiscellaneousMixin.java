cbaa1526fc551cbbc93ce28a387f692bf1ed0bac
genandnic <genandnic@gmail.com>
2020-02-12 20:41:35 -0600

    Removed sprintingTicks and CPlayerPacket stuff

--- src/main/java/genandnic/walljump/mixin/client/ClientPlayerEntityMiscellaneousMixin.java
+++ src/main/java/genandnic/walljump/mixin/client/ClientPlayerEntityMiscellaneousMixin.java
@@ -37,19 +37,19 @@ public class ClientPlayerEntityMiscellaneousMixin extends AbstractClientPlayerEn
             }
         }
 
+        // This can be omitted:
         // TODO: no idea where the sprintingTicksLeft logic is
         // if (pl.sprintingTicksLeft > 0 && pl.getMotion().length() > 0.08)
         //     pl.collidedHorizontally = false;
 
         if(this.fallDistance > 1.5 && !this.isFallFlying()) {
 
-            if(WallJump.CONFIGURATION.minFallDistance() > 127) {
-
-                this.fallDistance = 0.0F;
-                // TODO: no idea what the CPlayerPacket is
-                // pl.connection.sendPacket(new CPlayerPacket(true));
-
-            }
+//             This also can be omitted:
+//             if(WallJump.CONFIGURATION.minFallDistance() > 127) {
+//                 this.fallDistance = 0.0F;
+//                 // TODO: no idea what the CPlayerPacket is
+//                 // pl.connection.sendPacket(new CPlayerPacket(true));
+//             }
 
             if(WallJump.CONFIGURATION.playFallSound() && WallJumpClient.FALLING_SOUND.isDone()) {
 
