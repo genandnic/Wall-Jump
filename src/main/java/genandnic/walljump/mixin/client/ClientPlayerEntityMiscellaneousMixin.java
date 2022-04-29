2843b51b60edb4234ad57c89c96149fbb3a06eae
bee! <pascalyoung03@gmail.com>
2022-04-29 05:11:32 -0700

    add a missing import

--- src/main/java/genandnic/walljump/mixin/client/ClientPlayerEntityMiscellaneousMixin.java
+++ src/main/java/genandnic/walljump/mixin/client/ClientPlayerEntityMiscellaneousMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
